1642248d1cb6978807056630d54620c88ce458b2
Jasper Binda <jasper.binda@youngcolfield.nl>
2018-02-12 17:02:14 +0100

    entities

--- src/main/java/com/youngcolfield/battleship/domain/Account.java
+++ src/main/java/com/youngcolfield/battleship/domain/Account.java
@@ -3,20 +3,16 @@ package com.youngcolfield.battleship.domain;
 import lombok.Data;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
 @Entity
 public class Account {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private int type;
-
     private String email;
+
+    private int type;
     private String password;
     private String username;
-    
+
 }
