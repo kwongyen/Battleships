fe069e0e80e9508cd956cf70513761d05e193a22
kwongyen <kwongyen.mok@youngcolfield.nl>
2018-02-12 15:07:08 +0100

    register

--- src/main/java/com/youngcolfield/battleship/controller/AccountService.java
+++ src/main/java/com/youngcolfield/battleship/controller/AccountService.java
@@ -1,4 +1,20 @@
 package com.youngcolfield.battleship.controller;
 
+import com.youngcolfield.battleship.domain.Account;
+import com.youngcolfield.battleship.misc.RegisterVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account register(RegisterVO rvo){
+
+        return accountRepository.register(rvo);
+    }
 }
---
+++ src/main/java/com/youngcolfield/battleship/misc/RegisterVO.java
@@ -0,0 +1,10 @@
+package com.youngcolfield.battleship.misc;
+
+import lombok.Data;
+
+@Data
+public class RegisterVO {
+
+    private String firstName;
+    private String lastName;
+}
