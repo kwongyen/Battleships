7d60a5b464523c6fe26fbe11b6f8e4672465cb20
Jasper Binda <jasper.binda@youngcolfield.nl>
2018-02-14 14:29:13 +0100

    primary key email

--- src/main/java/com/youngcolfield/battleship/domain/Account.java
+++ src/main/java/com/youngcolfield/battleship/domain/Account.java
@@ -4,16 +4,20 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Data
 @Entity
 public class Account {
-
     @Id
     private String email;
-
-    private int type;
     private String password;
     private String username;
+
+    private int type;
+    private String bio;
+
+    @OneToMany
+    private Game game;
 
 }
--- src/main/java/com/youngcolfield/battleship/misc/AccountLoginId.java
+++ src/main/java/com/youngcolfield/battleship/misc/AccountLoginId.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class AccountLoginId {
-  private long id;
+  private String id;
 }
