70f7a154708022b805dccf4cbcb540e75681a909
Jasper Binda <jasper.binda@youngcolfield.nl>
2018-02-15 17:05:03 +0100

    some message queries

--- src/main/java/com/youngcolfield/battleship/controller/MessageRepository.java
+++ src/main/java/com/youngcolfield/battleship/controller/MessageRepository.java
@@ -6,15 +6,17 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface MessageRepository extends CrudRepository<Message, Long> {
 
     @Query("select m from Message m where m.sender = :id")
     Message findMessagesByGameid(@Param("id") Long id);
 
-    @Query("select m from Message m where m.receiver = :id")
-    Message findReceivedMessagesByEmail(@Param("id") String id);
+    @Query("select m.message from Message m where m.receiver = :id")
+    List<String> findReceivedMessagesByEmail(@Param("id") String id);
 
-    @Query("select m from Message m where m.sender = :id")
-    Message findSentMessagesByEmail(@Param("id") String id);
+    @Query("select m.message from Message m where m.sender = :id")
+    List<String> findSentMessagesByEmail(@Param("id") String id);
 }
